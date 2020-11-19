349d522a2d6173e028320cb026b9d4a62a3fb059
ANMOL AGARWAL <24794539+fineanmol@users.noreply.github.com>
2020-11-20 03:03:26 +0530

    ch2 complete

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,35 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Awesome App"),
-      ),
-      body: Container(
-        child: Center(child: Text("Hi FLutter")),
-      ),
-    );
+        appBar: AppBar(
+          title: Text("Awesome App"),
+        ),
+        body: Center(
+          child: Container(
+            padding: const EdgeInsets.all(8),
+            alignment: Alignment.center,
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(10),
+                boxShadow: [
+                  BoxShadow(
+                      color: Colors.grey[400],
+                      blurRadius: 5,
+                      spreadRadius: 5,
+                      offset: Offset(2.0, 5.0))
+                ],
+                color: Colors.teal,
+                gradient: LinearGradient(colors: [Colors.yellow, Colors.pink])),
+            width: 100,
+            height: 100,
+            child: Text(
+              "I am a box",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 20),
+            ),
+          ),
+        ));
   }
 }
